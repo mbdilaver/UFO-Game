96e4abeabedeb21c9aae5bdc924c5213b7269ff5
mbdilaver <mbdilaver@gmail.com>
2016-02-21 11:27:24 +0200

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Follow camera should always be implemented in LateUpdate because it tracks objects that might have moved inside Update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
